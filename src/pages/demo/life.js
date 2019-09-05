17d3eed9fe4b9ba122941433cf79ecdb9f373002
pybyongbo-pc <pyb901214@163.com>
2019-09-05 20:59:33 +0800

    first commit

---
+++ src/pages/demo/life.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import Child from './Child';
+import './index.less'
+export default class Life extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            count: 0
+        }
+    }
+
+    // state = {
+    //     count:0
+    // }
+
+    handClick() {
+        this.setState({
+            count: this.state.count - 1
+        })
+    }
+
+    handAdd = () => {
+        this.setState({
+            count: this.state.count + 1
+        })
+    }
+
+    render() {
+        return <div className="content">
+            <p>React生命周期介绍</p>
+            <button onClick={this.handAdd} style={{ marginRight: '20px' }}>加1</button>
+            <button onClick={this.handClick.bind(this)}>减1</button>
+            <p>{this.state.count}</p>
+            <Child name={this.state.count}></Child>
+        </div>
+    }
+
+}
