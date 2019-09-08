5e1f234228b4057df8ec6699ad442c6be174c8fa
pybyongbo-pc <pyb901214@163.com>
2019-09-08 22:24:35 +0800

    首页样式修改提交

---
+++ src/components/Header/index.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import {Row,Col} from 'antd';
+import './index.less';
+import Util from '../../utils/util';
+import axios from '../../axios/index';
+export default class Footer extends React.Component {
+    componentWillMount(){
+        this.setState({
+            username:'河畔一角'
+        });
+        setInterval(()=>{
+           let sysTime = Util.formateDate(new Date().getTime());
+           this.setState({
+            sysTime
+           })
+        },1000);
+        this.getWeatherAPIData();
+    }
+    getWeatherAPIData(){
+        let city = '上海';
+        axios.jsonp({
+            url:'http://api.map.baidu.com/telematics/v3/weather?location='+encodeURIComponent(city)+'&output=json&ak=3p49MVra6urFRGOT9s8UBWr2'
+        }).then((res)=>{
+            // console.log(res);
+            if(res.status ==='success') {
+                let data = res.results[0].weather_data[0];
+                this.setState({
+                    dayPictureUrl:data.dayPictureUrl,
+                    weather:data.weather
+                })
+            }
+        })
+    }
+    render() {
+        return (
+            <div className="header">
+                <Row className="header-top">
+                    <Col span={24}>
+                        <span>欢迎,{this.state.username}</span>
+                        <a href="">退出</a>
+                    </Col>
+                </Row>
+                <Row className="breadcrumb">
+                    <Col span={4} className="breadcrumb-title">
+                        首页
+                    </Col>
+                    <Col span={20} className="weather">
+                        <span className="date">{this.state.sysTime}</span>
+                        <span className="weather-img">
+                            <img src={this.state.dayPictureUrl} alt=""/>
+                           
+                        </span>
+                        <span className="weather-detail">{this.state.weather}</span>
+                    </Col>
+                </Row>   
+            </div>
+        )
+    }
+}
